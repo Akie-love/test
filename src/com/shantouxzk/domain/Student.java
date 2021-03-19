9d3d881af241b855c2dff03565ff89075068a739
zs <zs@bjpowernode.com>
2021-03-20 00:03:41 +0800

    456

--- src/com/shantouxzk/domain/Student.java
+++ src/com/shantouxzk/domain/Student.java
@@ -4,7 +4,7 @@ public class Student {
     private Integer id;
     private String name;
     private Integer age;
-    private String addr;
+    private String phone;
 
     public Student() {
     }
