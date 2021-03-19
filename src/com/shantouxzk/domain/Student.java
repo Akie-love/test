110d589d0de8a2b3dcb33421e59e6948f20fe200
xzk <xzk@shantouxzk.com>
2021-03-20 00:33:38 +0800

    test1_01

--- src/com/shantouxzk/domain/Student.java
+++ src/com/shantouxzk/domain/Student.java
@@ -6,6 +6,14 @@ public class Student {
     private Integer age;
     private String email;
 
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
     public Student() {
     }
 
